5f0723b0b225d5a5471d74d68de2fd64a951a4b0
Aleksandr Kuznetsov <akuzn1@gmail.com>
2018-03-31 13:36:34 +0400

    The initial version of the UniversalConverter was added. Functionally it's now equal to BasicConverter.

---
+++ Colorizer.Core.Test/Converters/UniversalConverterTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+using Colorizer.Core.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Colorizer.Core.Test.Converters
+{
+    [TestClass]
+    public class UniversalConverterTest
+    {
+        private Bitmap BasicInitBitmap()
+        {
+            var bitmap = new Bitmap(2, 2);
+            bitmap.SetPixel(0, 0, Color.FromArgb(255, 255, 0, 0));
+            bitmap.SetPixel(0, 1, Color.FromArgb(255, 0, 255, 0));
+            bitmap.SetPixel(1, 0, Color.FromArgb(255, 0, 0, 255));
+            bitmap.SetPixel(1, 1, Color.FromArgb(0, 0, 0, 0));
+            return bitmap;
+        }
+
+        [TestMethod]
+        public void ValidateUniversalConverter()
+        {
+            var bitmap = BasicInitBitmap();
+
+            UniversalConverter converter = new UniversalConverter
+            {
+                SourceColor = Color.FromArgb(255, 255, 0, 0),
+                DestinationColor = Color.FromArgb(255, 0, 0, 255)
+            };
+
+            Bitmap result = converter.Convert(bitmap);
+
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(0, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 255, 0).ToArgb(), result.GetPixel(0, 1).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(1, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(0, 0, 0, 0).ToArgb(), result.GetPixel(1, 1).ToArgb());
+        }
+
+        [TestMethod]
+        public void ValidateParametrizedConstructor()
+        {
+            var bitmap = BasicInitBitmap();
+
+            UniversalConverter converter = new UniversalConverter(Color.FromArgb(255, 255, 0, 0), Color.FromArgb(255, 0, 0, 255));
+
+            Bitmap result = converter.Convert(bitmap);
+
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(0, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 255, 0).ToArgb(), result.GetPixel(0, 1).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(1, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(0, 0, 0, 0).ToArgb(), result.GetPixel(1, 1).ToArgb());
+        }
+
+        [TestMethod]
+        public void ValidateEmptySourceColor()
+        {
+            var bitmap = BasicInitBitmap();
+            UniversalConverter converter = new UniversalConverter
+            {
+                // SourceColor is empty
+                DestinationColor = Color.FromArgb(255, 0, 0, 255)
+            };
+
+            Bitmap result = converter.Convert(bitmap);
+
+            Assert.AreEqual(Color.FromArgb(255, 255, 0, 0).ToArgb(), result.GetPixel(0, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 255, 0).ToArgb(), result.GetPixel(0, 1).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(1, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(1, 1).ToArgb());
+        }
+
+        [TestMethod]
+        public void ValidateEmptyDestinationColor()
+        {
+            var bitmap = BasicInitBitmap();
+
+            UniversalConverter converter = new UniversalConverter
+            {
+                SourceColor = Color.FromArgb(255, 255, 0, 0),
+                // DestinationColor is empty
+            };
+
+            Bitmap result = converter.Convert(bitmap);
+
+            Assert.AreEqual(Color.FromArgb(0, 0, 0, 0).ToArgb(), result.GetPixel(0, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 255, 0).ToArgb(), result.GetPixel(0, 1).ToArgb());
+            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255).ToArgb(), result.GetPixel(1, 0).ToArgb());
+            Assert.AreEqual(Color.FromArgb(0, 0, 0, 0).ToArgb(), result.GetPixel(1, 1).ToArgb());
+        }
+    }
+}
---
+++ Colorizer.Core/Converters/UniversalConverter.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Colorizer.Core.Converters
+{
+    /// <summary>
+    /// The universal converter for most typically cases.
+    /// </summary>
+    public class UniversalConverter : IBitmapConverter
+    {
+        public UniversalConverter() { }
+
+        public UniversalConverter(Color sourceColor, Color destinationColor)
+        {
+            SourceColor = sourceColor;
+            DestinationColor = destinationColor;
+        }
+
+        public Color SourceColor { get; set; }
+
+        public Color DestinationColor { get; set; }
+
+        public unsafe Bitmap Convert(Bitmap bitmapSource)
+        {
+            Bitmap result = new Bitmap(bitmapSource.Width, bitmapSource.Height);
+            BitmapData sourceData = bitmapSource.LockBits(new System.Drawing.Rectangle(0, 0, bitmapSource.Width, bitmapSource.Height),
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format32bppArgb);
+
+            BitmapData resultData = result.LockBits(new System.Drawing.Rectangle(0, 0, bitmapSource.Width, bitmapSource.Height),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format32bppArgb);
+            try
+            {
+                int length = bitmapSource.Width * bitmapSource.Height;
+                byte* sourcePointer = (byte*)(sourceData.Scan0);
+                byte* resultPointer = (byte*)(resultData.Scan0);
+                for (int i = 0; i < length; i++)
+                {
+                    if (*(sourcePointer) == SourceColor.B &&
+                        *(sourcePointer + 1) == SourceColor.G &&
+                        *(sourcePointer + 2) == SourceColor.R &&
+                        *(sourcePointer + 3) == SourceColor.A)
+                    {
+                        *resultPointer = DestinationColor.B;
+                        *(resultPointer + 1) = DestinationColor.G;
+                        *(resultPointer + 2) = DestinationColor.R;
+                        *(resultPointer + 3) = DestinationColor.A;
+                    }
+                    else
+                    {
+                        *resultPointer = *sourcePointer;
+                        *(resultPointer + 1) = *(sourcePointer + 1);
+                        *(resultPointer + 2) = *(sourcePointer + 2);
+                        *(resultPointer + 3) = *(sourcePointer + 3);
+                    }
+                    sourcePointer += 4;
+                    resultPointer += 4;
+                }
+            }
+            finally
+            {
+                bitmapSource.UnlockBits(sourceData);
+                result.UnlockBits(resultData);
+            }
+            return result;
+        }
+
+    }
+}
